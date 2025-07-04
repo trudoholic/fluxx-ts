033ffda4d876de7d69a73f6ea5b4c49b93fdc563
Oleg <35769958+trudoholic@users.noreply.github.com>
2025-07-04 21:37:29 +0300

    PageMain rf

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,3 @@
-import {Button, Heading, HStack, Separator} from "@chakra-ui/react"
-import {RiArrowLeftSFill, RiArrowRightSFill} from "react-icons/ri"
 import {GameState} from "../data/game"
 import useGame from "../hooks/useGame"
 
@@ -9,46 +7,11 @@ import PageOutro from "./PageOutro"
 
 const Main = () => {
   const {
-    gameState, testGameState,
-    phase, testPhase,
-    count, incCount, decCount,
+    gameState,
   } = useGame()
 
   return (
     <>
-      <HStack m={2}>
-        <Button
-          colorPalette={"orange"}
-          onClick={() => decCount(1)}
-        ><RiArrowLeftSFill /></Button>
-        <Button
-          colorPalette={"orange"}
-          onClick={() => incCount(1)}
-        ><RiArrowRightSFill /></Button>
-        <Heading as="h1">
-          count is {count}
-        </Heading>
-      </HStack>
-      <Separator width="100%" borderColor={'green.800'} />
-
-      <HStack m={2}>
-        <Button
-          colorPalette={"green"}
-          onClick={() => testGameState()}
-        ><RiArrowLeftSFill /></Button>
-        <Heading as="h1">
-          game state: {gameState}
-        </Heading>
-
-        <Button
-          colorPalette={"green"}
-          onClick={() => testPhase()}
-        ><RiArrowRightSFill /></Button>
-        <Heading as="h1">
-          phase: {phase}
-        </Heading>
-      </HStack>
-      <Separator width="100%" borderColor={'green.800'} />
       {
         GameState.Intro === gameState? (
           <PageIntro />
