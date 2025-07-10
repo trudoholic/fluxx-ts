83d064035b6134b0ac7779d458ece71f30528e32
Oleg <35769958+trudoholic@users.noreply.github.com>
2025-07-10 22:06:17 +0300

    scores

--- src/components/PageOutro.tsx
+++ src/components/PageOutro.tsx
@@ -1,14 +1,36 @@
-import {Button, Heading, HStack} from "@chakra-ui/react"
+import {Box, Button, Center, Heading, HStack, Separator} from "@chakra-ui/react"
 import {RiArrowRightSFill} from "react-icons/ri"
 import useGame from "../hooks/useGame"
 
 const PageOutro = () => {
   const {
     gameEnd,
+    players,
   } = useGame()
+
+  const scores = players.map(p => p.score)
+  const maxIdx = scores.indexOf(Math.max(...scores))
+  const minIdx = scores.indexOf(Math.min(...scores))
 
   return (
     <>
+      <HStack m={2}>
+        {
+          players.map((p, idx) => (
+            <Center
+              key={p.id} w="100px" h="40px" color="white" rounded="md"
+              bg={maxIdx === idx? "red.500": minIdx === idx? "blue.500": "green.700"}
+            >
+              <Box as="span" fontWeight="bold" fontSize="lg">
+                {`${p.name}: ${p.score}`}
+              </Box>
+            </Center>
+          ))
+        }
+      </HStack>
+      {/*---*/}
+      <Separator width="100%" borderColor={'green.800'} />
+      {/*---*/}
       <HStack m={2}>
         <Button
           colorPalette={"green"}
